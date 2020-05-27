5e15d8f98d4aebe450c6d9b94fcd27f85a9058f6
pamAdmin <>
2020-05-27 18:59:57 +0530

    {/src/main/java/com/myspace/reinsuranceworkflow/NomineeDetail.java}

--- src/main/java/com/myspace/reinsuranceworkflow/NomineeDetail.java
+++ src/main/java/com/myspace/reinsuranceworkflow/NomineeDetail.java
@@ -6,12 +6,70 @@ package com.myspace.reinsuranceworkflow;
 
 public class NomineeDetail implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public NomineeDetail() {
-    }
+	@org.kie.api.definition.type.Label(value = "Nominee Name")
+	private java.lang.String nomineeName;
+	@org.kie.api.definition.type.Label(value = "Date of Birth")
+	private java.util.Date dob;
+	@org.kie.api.definition.type.Label(value = "Relationship with Nominee")
+	private java.lang.String relationship;
+	@org.kie.api.definition.type.Label(value = "Percentage Share")
+	private java.lang.Double percentShare;
+	@org.kie.api.definition.type.Label(value = "Gender")
+	private java.lang.String gender;
 
+	public NomineeDetail() {
+	}
 
+	public java.lang.String getNomineeName() {
+		return this.nomineeName;
+	}
 
+	public void setNomineeName(java.lang.String nomineeName) {
+		this.nomineeName = nomineeName;
+	}
+
+	public java.util.Date getDob() {
+		return this.dob;
+	}
+
+	public void setDob(java.util.Date dob) {
+		this.dob = dob;
+	}
+
+	public java.lang.String getRelationship() {
+		return this.relationship;
+	}
+
+	public void setRelationship(java.lang.String relationship) {
+		this.relationship = relationship;
+	}
+
+	public java.lang.Double getPercentShare() {
+		return this.percentShare;
+	}
+
+	public void setPercentShare(java.lang.Double percentShare) {
+		this.percentShare = percentShare;
+	}
+
+	public java.lang.String getGender() {
+		return this.gender;
+	}
+
+	public void setGender(java.lang.String gender) {
+		this.gender = gender;
+	}
+
+	public NomineeDetail(java.lang.String nomineeName, java.util.Date dob,
+			java.lang.String relationship, java.lang.Double percentShare,
+			java.lang.String gender) {
+		this.nomineeName = nomineeName;
+		this.dob = dob;
+		this.relationship = relationship;
+		this.percentShare = percentShare;
+		this.gender = gender;
+	}
 
 }
